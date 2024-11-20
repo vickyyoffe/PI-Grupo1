f72fd8ee5133775d251b04aa0b3e009b9dd5a945
julimielnicki <jmielnicki@udesa.edu.ar>
2024-11-20 15:36:56 -0300

    Terminé el punto de register

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const userController = {
     registerPost: function (req, res) {  
         let form = req.body;
     
-        // Validación de los campos para que sean obligatorios
+        // Validaciones
         if (!form.email) {
             return res.send('El campo "email" no puede estar vacío.'); 
         } else if (!form.contrasenia) {
@@ -17,19 +17,28 @@ const userController = {
             return res.send('El campo "nombre" no puede estar vacío.'); 
         }
     
-        // Para que la contraseña quede encriptada
-        form.contrasenia = bcryptjs.hashSync(form.contrasenia, 10);
-    
-        db.Usuario.create(form) 
+        // Verificamos si el email  existe
+        db.Usuario.findOne({ where: { email: form.email } })
             .then(function (results) {
+                if (results) {
+                    // Si el email ya existe, enviar mensaje al usuario
+                    return res.send('El email ingresado ya está registrado. Intente con otro.');
+                }
+    
+                // Si no existe, encriptamos la contraseña y creamos el usuario
+                form.contrasenia = bcryptjs.hashSync(form.contrasenia, 10);
+    
+                return db.Usuario.create(form);  // Ahora creamos el usuario
+            })
+            .then(function () {
                 return res.redirect("/users/login"); 
             })
             .catch(function (err) {
-                console.error("Error al registrar el usuario:", err); // Usa console.error para resaltarlo
+                console.log("Error al registrar el usuario:", err); 
                 return res.send("Hubo un problema al registrar el usuario.");
             });
-            
     }
+    
     
 }
 
