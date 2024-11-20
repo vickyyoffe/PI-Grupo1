41d4f3e0cdcaeec4eff8e11891deba7496f330ba
pilarantunez <pantunezmunoz@udesa.edu.ar>
2024-11-20 16:29:20 -0300

    home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,27 +1,24 @@
-const { Association } = require('sequelize');
-const db = require('../database/models'); // cambio lo que estaba de db por models
-const op = db.Sequelize.Op
+const db = require('../database/models'); // Reemplazo por models correctamente
+const op = db.Sequelize.Op;
 
 const indexController = {
   index: function (req, res) {
+    db.Producto.findAll({
+      include: [
+        {
+          model: db.Usuario, // Se usa el modelo asociado correctamente
+          as: 'usuarios', // Asegúrate de usar el alias definido en las relaciones
+        },
+      ],
+      order: [["createdAt", "DESC"]],
+    })
+      .then(function (results) {
+        return res.render("index", { lista: results }); // Paso 'lista' a la vista
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  },
+};
 
-  let filtrado = {
-          include:[
-            {model: "usuarios"}],
-          order:[
-            ["createdAt", "DESC"]
-            ]}
-
-    db.Producto.findAll(filtrado)
-
-    .then(function(results) {
-      return res.send(results)
-       return res.render("/", {lista: results})
-   }).catch(function(err) {
-      return console.log(err);
-   });
-
-  }
-
-}
 module.exports = indexController;
